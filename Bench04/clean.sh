8243654212a14b65f2c15d3fb023105e1bb4d640
Pedro Costa <phtcosta@gmail.com>
2022-03-30 10:06:41 -0300

    configuring profiles

--- Bench04/clean.sh
+++ Bench04/clean.sh
@@ -2,14 +2,14 @@
 
 # delete 'output' folders
 
-rm -Rf apache_codes/activemq-artemis/artemis-commons/output
-rm -Rf apache_codes/deltaspike/deltaspike/core/impl/output
-rm -Rf apache_codes/directory-server/kerberos-codec/output
-rm -Rf apache_codes/manifoldcf/framework/core/output
-rm -Rf apache_codes/openwebbeans-meecrowave/meecrowave-core/output
-rm -Rf apache_codes/spark/core/output
-rm -Rf apache_codes/tika/tika-core/output
-#rm -Rf apache_codes/tomee/container/openejb-core/output
-rm -Rf apache_codes/wicket/wicket-util/output
+rm -Rfv apache_codes/activemq-artemis/artemis-commons/output
+rm -Rfv apache_codes/deltaspike/deltaspike/core/impl/output
+rm -Rfv apache_codes/directory-server/kerberos-codec/output
+rm -Rfv apache_codes/manifoldcf/framework/core/output
+rm -Rfv apache_codes/openwebbeans-meecrowave/meecrowave-core/output
+rm -Rfv apache_codes/spark/core/output
+rm -Rfv apache_codes/tika/tika-core/output
+#rm -Rfv apache_codes/tomee/container/openejb-core/output
+rm -Rfv apache_codes/wicket/wicket-util/output
 
 
