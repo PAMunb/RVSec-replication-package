0aa5205b093a58f0c5204abbaa765226c69dc403
Pedro Costa <phtcosta@gmail.com>
2022-03-30 09:21:09 -0300

    jacoco results

---
+++ Bench04/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# delete 'output' folders
+
+rm -Rf apache_codes/activemq-artemis/artemis-commons/output
+rm -Rf apache_codes/deltaspike/deltaspike/core/impl/output
+rm -Rf apache_codes/directory-server/kerberos-codec/output
+rm -Rf apache_codes/manifoldcf/framework/core/output
+rm -Rf apache_codes/openwebbeans-meecrowave/meecrowave-core/output
+rm -Rf apache_codes/spark/core/output
+rm -Rf apache_codes/tika/tika-core/output
+#rm -Rf apache_codes/tomee/container/openejb-core/output
+rm -Rf apache_codes/wicket/wicket-util/output
+
+
