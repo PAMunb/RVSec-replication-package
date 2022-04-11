87dddd316629ea268e48d264a8b90d5b9fcca8bb
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-04-11 09:45:16 -0300

    false negative assessment

---
+++ CryptoAPIBench/asset/cg-false-negatives/CredentialInStringABMCCase1.java
@@ -0,0 +1,10 @@
+import java.security.SecureRandom;
+
+public class CredentialInStringABMCCase1 {
+    public static void main(String [] args){
+        CredentialInStringABMC1 pc = new CredentialInStringABMC1();
+        SecureRandom random = new SecureRandom();
+        String key = String.valueOf(random.ints());
+        pc.go(key);
+    }
+}
