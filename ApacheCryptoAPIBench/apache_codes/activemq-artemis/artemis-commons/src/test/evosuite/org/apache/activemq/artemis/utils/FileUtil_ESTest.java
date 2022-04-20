cf0be3185d3830b9ce8d7cd17027bbb317f2e2d1
Pedro Costa <phtcosta@gmail.com>
2022-04-20 09:22:02 -0300

    configuring evosuite

--- ApacheCryptoAPIBench/apache_codes/activemq-artemis/artemis-commons/src/test/evosuite/org/apache/activemq/artemis/utils/FileUtil_ESTest.java
+++ ApacheCryptoAPIBench/apache_codes/activemq-artemis/artemis-commons/src/test/evosuite/org/apache/activemq/artemis/utils/FileUtil_ESTest.java
@@ -18,74 +18,76 @@ import org.evosuite.runtime.EvoRunnerParameters;
 import org.evosuite.runtime.mock.java.io.MockFile;
 import org.junit.runner.RunWith;
 
-@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true) 
+@RunWith(EvoRunner.class)
+@EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true)
 public class FileUtil_ESTest extends FileUtil_ESTest_scaffolding {
 
-  @Test(timeout = 4000)
-  public void test0()  throws Throwable  {
-      MockFile mockFile0 = new MockFile("", "");
-      MockFile mockFile1 = new MockFile("60/)sRn;j`]R_>");
-      mockFile1.mkdirs();
-      // Undeclared exception!
-      FileUtil.deleteDirectory(mockFile0);
-  }
+	@Test(timeout = 4000)
+	public void test0() throws Throwable {
+		MockFile mockFile0 = new MockFile("", "");
+		MockFile mockFile1 = new MockFile("60/)sRn;j`]R_>");
+		mockFile1.mkdirs();
+		// Undeclared exception!
+		FileUtil.deleteDirectory(mockFile0);
+	}
 
-  @Test(timeout = 4000)
-  public void test1()  throws Throwable  {
-      MockFile mockFile0 = new MockFile("");
-      boolean boolean0 = mockFile0.setReadOnly();
-      MockFile mockFile1 = new MockFile("", "");
-      boolean boolean1 = FileUtil.deleteDirectory(mockFile1);
-      assertFalse(boolean1 == boolean0);
-  }
+	@Test(timeout = 4000)
+	public void test1() throws Throwable {
+		MockFile mockFile0 = new MockFile("");
+		boolean boolean0 = mockFile0.setReadOnly();
+		MockFile mockFile1 = new MockFile("", "");
+		boolean boolean1 = FileUtil.deleteDirectory(mockFile1);
+		assertFalse(boolean1 == boolean0);
+	}
 
-  @Test(timeout = 4000)
-  public void test2()  throws Throwable  {
-      MockFile mockFile0 = new MockFile("", "");
-      // Undeclared exception!
-      try { 
-        FileUtil.deleteDirectory(mockFile0);
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-      }
-  }
+	@Test(timeout = 4000)
+	public void test2() throws Throwable {
+		MockFile mockFile0 = new MockFile("", "");
+		// Undeclared exception!
+		try {
+			FileUtil.deleteDirectory(mockFile0);
+			fail("Expecting exception: NullPointerException");
 
-  @Test(timeout = 4000)
-  public void test3()  throws Throwable  {
-      MockFile mockFile0 = new MockFile("");
-      boolean boolean0 = FileUtil.deleteDirectory(mockFile0);
-      assertTrue(boolean0);
-  }
+		} catch (NullPointerException e) {
+			//
+			// no message in exception (getMessage() returned null)
+			//
+		}
+	}
 
-  @Test(timeout = 4000)
-  public void test4()  throws Throwable  {
-      MockFile mockFile0 = new MockFile("Is dvFHrCX Yh)`Bx", "r9WlqZZwfK;,V1<v");
-      boolean boolean0 = FileUtil.deleteDirectory(mockFile0);
-      assertFalse(boolean0);
-  }
+	@Test(timeout = 4000)
+	public void test3() throws Throwable {
+		MockFile mockFile0 = new MockFile("");
+		boolean boolean0 = FileUtil.deleteDirectory(mockFile0);
+		assertTrue(boolean0);
+	}
 
-  @Test(timeout = 4000)
-  public void test5()  throws Throwable  {
-    Future<?> future = executor.submit(new Runnable(){ 
-            @Override public void run() { 
-        try {
-          MockFile mockFile0 = new MockFile("");
-          FileUtil.makeExec(mockFile0);
-          assertEquals(0L, mockFile0.getUsableSpace());
-        } catch(Throwable t) {
-            // Need to catch declared exceptions
-        }
-      } 
-    });
-    future.get(4000, TimeUnit.MILLISECONDS);
-  }
+	@Test(timeout = 4000)
+	public void test4() throws Throwable {
+		MockFile mockFile0 = new MockFile("Is dvFHrCX Yh)`Bx", "r9WlqZZwfK;,V1<v");
+		boolean boolean0 = FileUtil.deleteDirectory(mockFile0);
+		assertFalse(boolean0);
+	}
 
-  @Test(timeout = 4000)
-  public void test6()  throws Throwable  {
-      FileUtil fileUtil0 = new FileUtil();
-  }
+	@Test(timeout = 4000)
+	public void test5() throws Throwable {
+		Future<?> future = executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				MockFile mockFile0 = new MockFile("");
+				try {
+					FileUtil.makeExec(mockFile0);
+				} catch (Throwable t) {
+					// Need to catch declared exceptions
+				}
+				assertEquals(0L, mockFile0.getUsableSpace());
+			}
+		});
+		future.get(4000, TimeUnit.MILLISECONDS);
+	}
+
+	@Test(timeout = 4000)
+	public void test6() throws Throwable {
+		FileUtil fileUtil0 = new FileUtil();
+	}
 }
