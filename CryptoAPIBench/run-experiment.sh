ee01f93ec485ee676b78053f69598c0a0dbfc4ca
Jonata Pastro <jtpastro@gmail.com>
2022-11-22 13:02:16 -0300

    Refactoring run-experiments.sh

--- CryptoAPIBench/run-experiment.sh
+++ CryptoAPIBench/run-experiment.sh
@@ -4,44 +4,33 @@ if [[ -z "${JAVA_HOME}" ]]; then
 fi
 
 PATH_RVSEC=$(mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)/br/unb/cic
-
 if [[ ! -d $PATH_RVSEC ]]; then
 	../scripts/build_rvsec.sh
 fi
 
-echo "Running RVSec"
-
+echo "Startng RVSec"
+echo "Cleaning output"
 cd assets/cryptoAPIBench
-
 rm -f output/*.*
 
-mvn package 
-
+echo "Running RVSec"
+mvn package -q 
 mv output/summary.csv ../../results/mop.csv
-
 cd target
 
 echo "Running CogniCrypt"
-
 java -cp ../../../../tools/CryptoAnalysis.jar crypto.HeadlessCryptoScanner \
      --sarifReport --reportDir . \
      --rulesDir ../../../../tools/rules \
      --applicationCp cryptoAPIBench-1.0-SNAPSHOT.jar
-
 mv CogniCrypt-SARIF-Report.txt ../../../results/CryptoAnalysis-Report.json
 
 echo "Running CryptoGuard"
-
 java -jar ../../../../tools/cryptoguard-develop.jar  -in jar -s cryptoAPIBench-1.0-SNAPSHOT.jar -o CryptoGuard-Report.json
-
 mv CryptoGuard-Report.json ../../../results
-
 cd ../../../results
-
 python3 ../../scripts/process-cc-report.py CryptoAnalysis-Report.json cognicrypt.csv
-
 python3 ../../scripts/process-cg-report.py CryptoGuard-Report.json cryptoguard.csv
-
 rm CryptoAnalysis-Report.json
 rm CryptoGuard-Report.json
 
