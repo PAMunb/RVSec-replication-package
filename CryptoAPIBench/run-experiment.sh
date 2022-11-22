356816074b3a6200d4df57f521b00caaa5aaa26e
Jonata Pastro <jtpastro@gmail.com>
2022-11-22 13:27:00 -0300

    Sorting result files facilitating comparisons.

--- CryptoAPIBench/run-experiment.sh
+++ CryptoAPIBench/run-experiment.sh
@@ -8,14 +8,14 @@ if [[ ! -d $PATH_RVSEC ]]; then
 	../scripts/build_rvsec.sh
 fi
 
-echo "Startng RVSec"
+echo "Starting RVSec"
 echo "Cleaning output"
 cd assets/cryptoAPIBench
 rm -f output/*.*
 
 echo "Running RVSec"
 mvn package -q 
-mv output/summary.csv ../../results/mop.csv
+sort output/summary.csv > ../../results/mop.csv
 cd target
 
 echo "Running CogniCrypt"
@@ -28,9 +28,13 @@ mv CogniCrypt-SARIF-Report.txt ../../../results/CryptoAnalysis-Report.json
 echo "Running CryptoGuard"
 java -jar ../../../../tools/cryptoguard-develop.jar  -in jar -s cryptoAPIBench-1.0-SNAPSHOT.jar -o CryptoGuard-Report.json
 mv CryptoGuard-Report.json ../../../results
+
+echo "Standardizing CongniCrypt and CryptoGuard output"
 cd ../../../results
 python3 ../../scripts/process-cc-report.py CryptoAnalysis-Report.json cognicrypt.csv
+sort cognicrypt.csv > cognicrypt.csv
 python3 ../../scripts/process-cg-report.py CryptoGuard-Report.json cryptoguard.csv
+sort cryptoguard.csv > cryptoguard.csv
 rm CryptoAnalysis-Report.json
 rm CryptoGuard-Report.json
 
