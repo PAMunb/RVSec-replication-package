568b1628ed4b52f8cacf8e63ed54aa591cfd1c8b
Jonata Pastro <jtpastro@gmail.com>
2022-11-24 09:26:45 -0300

    Making run-experiment.sh self-contained

--- MASCBench/run-experiment.sh
+++ MASCBench/run-experiment.sh
@@ -1,37 +1,41 @@
-echo "Running RVSec"
+if [[ -z "${JAVA_HOME}" ]]; then
+	echo "Please set JAVA_HOME path before running the script."
+	exit -1
+fi
 
+PATH_RVSEC=$(mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)/br/unb/cic
+if [[ ! -d $PATH_RVSEC ]]; then
+	../scripts/build_rvsec.sh
+fi
+
+echo "Starting RVSec"
+echo "Cleaning output"
 cd assets/mascBench
-
 rm -f output/*.*
 
-mvn package --q
-
-mv output/summary.csv ../../results/mop.csv
-
+echo "Running RVSec"
+mvn package -q 
+sort output/summary.csv -o ../../results/mop.csv
 cd target
 
 echo "Running CogniCrypt"
-
-java -Xss4096m -Xmx12000m -Xms4096m \
+java -Xss4096m -Xmx12000m -Xms4096m \ 
      -cp ../../../../tools/CryptoAnalysis-3a78193.jar crypto.HeadlessCryptoScanner \
      --reportFormat SARIF --reportPath . \
      --rulesDir ../../../../tools/rules \
      --appPath mascBench-1.0-SNAPSHOT.jar
-
 mv CryptoAnalysis-Report.json ../../../results
 
 echo "Running CryptoGuard"
-
 java -jar ../../../../tools/cryptoguard-develop.jar  -in jar -s mascBench-1.0-SNAPSHOT.jar -o CryptoGuard-Report.json
-
 mv CryptoGuard-Report.json ../../../results
 
+echo "Standardizing CongniCrypt and CryptoGuard output"
 cd ../../../results
-
 python3 ../../scripts/process-cc-report.py CryptoAnalysis-Report.json cognicrypt.csv
-
+sort cognicrypt.csv -o cognicrypt.csv
 python3 ../../scripts/process-cg-report.py CryptoGuard-Report.json cryptoguard.csv
-
+sort cryptoguard.csv -o cryptoguard.csv
 rm CryptoAnalysis-Report.json
 rm CryptoGuard-Report.json
 
