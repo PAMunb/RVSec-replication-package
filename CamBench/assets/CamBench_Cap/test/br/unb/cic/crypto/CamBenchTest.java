ffb27a06da46031056bc8999d311948f52a65d86
Pedro Costa <phtcosta@gmail.com>
2023-05-29 09:42:30 -0300

    initial skeleton

---
+++ CamBench/assets/CamBench_Cap/test/br/unb/cic/crypto/CamBenchTest.java
@@ -0,0 +1,8 @@
+package br.unb.cic.crypto;
+
+public class CamBenchTest extends ReflectionBased {
+    @Override
+    protected String definePackage() {
+        return "main.java.org.cambench.cap";
+    }
+}
---
+++ CamBench/assets/readme.txt
@@ -0,0 +1,2 @@
+cloned from: https://github.com/CROSSINGTUD/CamBench/tree/main
+revision: 9af4a06506faf7ef1fdd130fe49e62f540d58994
